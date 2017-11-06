9c688b2421f3fa9ce31c5dcce49351913c08353f
vinh <12>
2017-11-06 21:00:52 +0700

    update

--- app/src/main/java/com/cvcompany/vo/dagger2/MyDagger2.java
+++ app/src/main/java/com/cvcompany/vo/dagger2/MyDagger2.java
@@ -9,5 +9,6 @@ public class MyDagger2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_dagger);
+        int a =1;
     }
 }
--- app/src/main/java/com/cvcompany/vo/dagger2/Test2.java
+++ app/src/main/java/com/cvcompany/vo/dagger2/Test2.java
@@ -4,6 +4,6 @@ package com.cvcompany.vo.dagger2;
  * Created by vinh on 11/6/2017.
  */
 
-public class Test {
-    int a;
+public class Test2 {
+    int b=2;
 }
